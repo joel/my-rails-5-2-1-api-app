7d1c6a7fd49c485bae2fec8188ceb9751a9cd9c5
Joel AZEMAR <joel.azemar@gmail.com>
2018-11-13 20:08:59 +0100

    Change Partial Create into Full Create

--- spec/api/v1/items/create_spec.rb
+++ spec/api/v1/items/create_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "items#create", type: :request do
-  let(:todo)    { create(:todo) }
+  let(:todo_id) { SecureRandom.uuid }
   let(:item_id) { SecureRandom.uuid }
+
+  before { Timecop.freeze(Time.local(1990)) }
   
   subject(:make_request) do
-    jsonapi_put "/api/v1/todos/#{todo.id}", payload
+    jsonapi_post "/api/v1/todos", payload
   end
 
   describe 'basic create' do
     let(:payload) do
       {
         data: {
-          id: todo.id.to_s,
+          id: todo_id.to_s,
           type: 'todos',
           attributes: {
             owner: 'John Doe',
@@ -39,7 +41,7 @@ RSpec.describe "items#create", type: :request do
     let(:full_response) do
       {
         "data": {
-          "id": "#{todo.id}",
+          "id": "#{todo_id}",
           "type": "todos",
           "attributes": {
             "title": "Bucket List",
@@ -64,11 +66,11 @@ RSpec.describe "items#create", type: :request do
               "title": "Tricycle",
               "done": false,
               "todo": {
-                "id": "#{todo.id}",
+                "id": "#{todo_id}",
                 "title": "Bucket List",
                 "owner": "John Doe",
-                "created_at": "2018-11-11T19:42:27.179Z",
-                "updated_at": "2018-11-11T19:42:27.195Z"
+                "created_at": "1989-12-31T23:00:00.000Z",
+                "updated_at": "1989-12-31T23:00:00.000Z"
               }
             }
           }
@@ -85,9 +87,13 @@ RSpec.describe "items#create", type: :request do
     it 'creates the resource' do
       expect {
         make_request
-      }.to change { Item.count }.by(1)
-      
-      expect(body_response['data']).to include(JSON.load(full_response.to_json)['data'])
+      }.to change { 
+        Item.count 
+      }.by(1).and change { 
+        Todo.count 
+      }.by(1)
+binding.pry
+      expect(body_response).to eql(JSON.load(full_response.to_json))
     end
   end
 end
